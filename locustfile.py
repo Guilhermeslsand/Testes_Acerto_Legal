618f424ec5ff3b54ec633dc8bdd5c052f80a6f9c
guilherme <guilhermeslandrade@gmail.com>
2025-11-26 11:54:32 -0300

    Ajuste no ste rag chat

--- locustfile.py
+++ locustfile.py
@@ -7,10 +7,10 @@ class ChatApiUser(HttpUser):
     def criar_thread(self):
         self.client.post("/rag-chat/create-thread-v2")
 
-    @task
-    def consultar_historico(self):
-        self.client.get("/rag-chat/historico/v2", params={"thread_id": "thread_25f587fbd0a1_1764077429"})
+    # @task
+    # def consultar_historico(self):
+    #     self.client.get("/rag-chat/historico/v2", params={"thread_id": "thread_25f587fbd0a1_1764077429"})
 
-    @task
-    def estatisticas_sessao(self):
-        self.client.get("/rag-chat/session/stats")
+    # @task
+    # def estatisticas_sessao(self):
+    #     self.client.get("/rag-chat/session/stats")
